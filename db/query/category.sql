4d7232a0b20cae39f1bc4b12b8e297b3589eebe7
SraReaper <carolin.cosgirl@gmail.com>
2024-12-03 14:25:44 -0400

    created new queries to pick up specific points

--- db/query/category.sql
+++ db/query/category.sql
@@ -14,6 +14,15 @@ SELECT * FROM categories WHERE id = $1 LIMIT 1;
 -- name: GetCategories :many
 SELECT * FROM categories WHERE user_id = $1 AND type = $2 AND title LIKE $3 AND description LIKE $4;
 
+-- name: GetCategoriesByUserIdAndType :many
+SELECT * FROM categories WHERE user_id = $1 AND type = $2;
+
+-- name: GetCategoriesByUserIdAndTypeAndTitle :many
+SELECT * FROM categories WHERE user_id = $1 AND type = $2 AND title LIKE $3;
+
+-- name: GetCategoriesByUserIdAndTypeAndDescription :many
+SELECT * FROM categories WHERE user_id = $1 AND type = $2 AND description LIKE $3;
+
 -- name: UpdateCategory :one
 UPDATE categories SET title = $2, description = $3 WHERE id = $1 RETURNING *;
 
