ddd84d2455d151b449a7a6eaec617e5eb003779a
SraReaper <carolin.cosgirl@gmail.com>
2025-01-28 15:19:52 -0400

    refactor: removido as query sem utilidad e modificado nome de y para ies

--- db/query/category.sql
+++ db/query/category.sql
@@ -12,19 +12,10 @@ INSERT INTO categories (
 SELECT * FROM categories WHERE id = $1 LIMIT 1;
 
 -- name: GetCategories :many
-SELECT * FROM categories WHERE user_id = $1 AND type = $2 AND title LIKE CONCAT('%', sqlc.arg('title')::text, '%') AND description LIKE CONCAT('%', sqlc.arg('description')::text, '%');
+SELECT * FROM categories WHERE user_id = $1 AND type = $2 AND LOWER(title) LIKE CONCAT('%', LOWER(sqlc.arg('title')::text), '%') AND LOWER(description) LIKE CONCAT('%', LOWER(sqlc.arg('description')::text), '%');
 
--- name: GetCategoriesByUserIdAndType :many
-SELECT * FROM categories WHERE user_id = $1 AND type = $2;
-
--- name: GetCategoriesByUserIdAndTypeAndTitle :many
-SELECT * FROM categories WHERE user_id = $1 AND type = $2 AND title LIKE $3;
-
--- name: GetCategoriesByUserIdAndTypeAndDescription :many
-SELECT * FROM categories WHERE user_id = $1 AND type = $2 AND description LIKE $3;
-
--- name: UpdateCategory :one
+-- name: UpdateCategories :one
 UPDATE categories SET title = $2, description = $3 WHERE id = $1 RETURNING *;
 
--- name: DeleteCategory :exec
+-- name: DeleteCategories :exec
 DELETE FROM categories WHERE id = $1;
